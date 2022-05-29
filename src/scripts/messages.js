91d02cc65d8a9392df547be1b9ab24022aafc9c8
Ana Ferreira <anapaulaferreira0309@gmail.com>
2022-05-29 15:12:33 -0300

    update messages.js

--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -3,10 +3,16 @@
 // Ao completar o desenho da forca, deve ser exibida uma mensagem na tela de "Fim de Jogo"
 // Se completar a palavra correta antes de acabarem as tentativas, deve ser exibida na tela a mensagem "Você Venceu. Parabéns!"
 
-function messageText(status) {
+function messageText(text, status) {
   let messageCard = document.querySelector('.display #message-card');
+  let getLetter = document.querySelector('.display #enterLetter');
 
-  return status === 'win' ? (messageCard.value = 'Você venceu!') : (messageCard.value = 'Você perdeu!');
+  messageCard.classList.add('active');
+  messageCard.innerHTML = `${text}`;
+
+  if (status === 'Aviso') {
+    getLetter.style.borderColor = 'red';
+  }
 }
 
 export { messageText };
